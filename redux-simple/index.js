55931a282c30cc22825db7aee530ce06bbae982d
George <admin@georgesimos.com>
2020-03-17 21:25:16 +0200

    feat(redux-simple): combineReducers

--- redux-simple/index.js
+++ redux-simple/index.js
@@ -18,8 +18,8 @@ const makeLouderAndBoldAndRepeatThreeTimes = compose(
 
 console.log(makeLouderAndBoldAndRepeatThreeTimes("simos"));
 
-const reducer = (state = { value: 1 }, action) => {
-  console.log("Something happened!", action);
+const calculatorReducer = (state = { value: 1 }, action) => {
+  console.log("calculatorReducer: ", action);
   if (action.type === "ADD") {
     const value = state.value;
     const amount = action.payload.amount;
@@ -27,6 +27,19 @@ const reducer = (state = { value: 1 }, action) => {
   }
   return state;
 };
+
+const errorReducer = (state = { error: null }, action) => {
+  console.log("errorReducer: ", action);
+  if (action.type === "ERROR") {
+    return { ...state, error: action.payload };
+  }
+  return state;
+};
+
+const reducer = combineReducers({
+  calculator: calculatorReducer,
+  error: errorReducer
+});
 
 const store = createStore(reducer);
 
@@ -37,6 +50,11 @@ const unsubscribe = store.subscribe(() => {
 
 store.dispatch({ type: "ADD", payload: { amount: 2 }, meta: {} });
 store.dispatch({ type: "ADD", payload: { amount: 2 }, meta: {} });
+store.dispatch({
+  type: "ERROR",
+  payload: { message: "This is an error" },
+  meta: {}
+});
 
 unsubscribe();
 
