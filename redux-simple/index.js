1ffb3653d1e30ba319920c4a56538596dcdbb926
George <admin@georgesimos.com>
2020-03-17 20:59:01 +0200

    feat(redux-simple): compose

--- redux-simple/index.js
+++ redux-simple/index.js
@@ -1,11 +1,19 @@
-import {
+const {
   createStore,
   combineReducers,
   compose,
   bindActionCreators,
   applyMiddleware
-} from "redux";
+} = require("redux");
 
 const makeLouder = string => string.toUpperCase();
 const repeatTreeTimes = string => string.repeat(3);
 const embolden = string => string.bold();
+
+const makeLouderAndBoldAndRepeatThreeTimes = compose(
+  embolden,
+  repeatTreeTimes,
+  makeLouder
+);
+
+console.log(makeLouderAndBoldAndRepeatThreeTimes("simos"));
