4874311887b9a325f33ef5ecb4b06055c6b7f7ae
George <admin@georgesimos.com>
2020-03-18 09:56:59 +0200

    creating a simple redux store

--- redux-react/redux-counter/src/index.js
+++ redux-react/redux-counter/src/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
 import './styles.scss';
+
+const initialState = { count: 0 };
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+const decrementValue = () => ({
+  type: DECREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return { count: state.count + 1 };
+  }
+
+  if (action.type === DECREMENT) {
+    return { count: state.count - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
